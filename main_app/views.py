ca87f303313f75e2075c60f8873e9b7e817d3830
nununuam <nunusnuam@gmail.com>
2022-05-19 23:13:06 -0700

    added the classes

--- main_app/views.py
+++ main_app/views.py
@@ -7,5 +7,53 @@ from django.views.generic.base import TemplateView
 class Home(TemplateView):
     template_name = "home.html"
 
+class AuthorStatement(TemplateView):
+    template_name = "authorStatement.html"
+
 class Button1(TemplateView):
     template_name = "button1.html"
+
+class Button2(TemplateView):
+    template_name = "button2.html"
+
+class Button3(TemplateView):
+    template_name = "button3.html"
+
+class Button4(TemplateView):
+    template_name = "button4.html"
+
+class Button5(TemplateView):
+    template_name = "button5.html"
+
+class Button6(TemplateView):
+    template_name = "button6.html"
+
+class Button7(TemplateView):
+    template_name = "button7.html"
+
+class Button8(TemplateView):
+    template_name = "button8.html"
+
+class Button9(TemplateView):
+    template_name = "button9.html"
+
+class Button10(TemplateView):
+    template_name = "button10.html"
+
+class Button11(TemplateView):
+    template_name = "button11.html"
+
+class Button12(TemplateView):
+    template_name = "button12.html"
+
+class Button13(TemplateView):
+    template_name = "button13.html"
+
+class Button14(TemplateView):
+    template_name = "button14.html"
+
+class Button15(TemplateView):
+    template_name = "button15.html"
+
+class Button16(TemplateView):
+    template_name = "button16.html"
