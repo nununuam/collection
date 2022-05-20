5ba41e663053f91a545d3fb04fb2fb943cffb7a7
nununuam <nunusnuam@gmail.com>
2022-05-19 23:00:48 -0700

    initial button url and view

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
+    path('button1/', views.Button1.as_view(), name="button1"),
     
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,6 @@ from django.views.generic.base import TemplateView
 
 class Home(TemplateView):
     template_name = "home.html"
+
+class Button1(TemplateView):
+    template_name = "button1.html"
